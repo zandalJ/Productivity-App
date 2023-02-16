ca24f69378cfc3b139c697e6e7938bc39f902cf6
Fabian <zandal.zdl@gmail.com>
2023-02-16 21:21:48 +0100

    Changed get started button as a link

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,15 +2,18 @@ import styles from './HomePage.module.scss'
 import Wrapper from "../Components/ui/Wrapper";
 import Button from '../Components/ui/Button';
 import header from '../img/header.svg'
+import { Link } from 'react-router-dom';
 const HomePage = () => {
 	return (
 		<Wrapper center>
 			<div className={styles.box}>
 				<h1>Best productivity app for everyone!</h1>
 				<p>Increase your efficiency</p>
-				<Button>Get started</Button>
+				<Link to="/test">
+					<Button>Get started</Button>
+				</Link>
 			</div>
-			<img src={header} alt="header" className={styles.img}/>
+			<img src={header} alt='header' className={styles.img} />
 		</Wrapper>
 	);
 };
