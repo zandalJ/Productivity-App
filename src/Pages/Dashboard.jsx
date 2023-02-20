50cc938aa7e0cc5d17621423836c1f8ed28c97e9
Fabian <zandal.zdl@gmail.com>
2023-02-20 20:33:07 +0100

    Added tasks & habbits stats section to dashboard page

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -2,12 +2,14 @@ import styles from "./Dashboard.module.scss";
 import Wrapper from "../Components/ui/Wrapper";
 import HeadingText from "../Components/ui/HeadingText";
 import Card from "../Components/ui/Card";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
 
 const DashboardPage = () => {
 	return (
 		<Wrapper>
 			<HeadingText />
-			<section className={`center-column-x ${styles.growth}`}>
+			<section className={`${styles.section} ${styles.growth}`}>
 				<Card className={styles.card}>
 					<div className={styles["growth__header-box"]}>
 						<p
@@ -27,7 +29,7 @@ const DashboardPage = () => {
 					<div className={styles["growth__header-box"]}>
 						<p
 							className={`${styles["growth__text"]} ${styles["growth__text--green"]}`}>
-							Tasks Completed
+							New Tasks
 						</p>
 						<p className={styles["growth__number-count"]}>07</p>
 					</div>
@@ -36,6 +38,61 @@ const DashboardPage = () => {
 							+22%
 						</span>
 						more from the last week
+					</p>
+				</Card>
+			</section>
+			<section className={`${styles.stats} ${styles.section}`}>
+				<h2>Tasks & Habits</h2>
+				<Card className={styles["stats__box"]}>
+					<div className={styles["stats__box--text"]}>
+						<FontAwesomeIcon
+							icon={solid("infinity")}
+							className={styles["stats__icon"]}
+						/>
+						<p>All Tasks</p>
+					</div>
+					<p
+						className={`${styles["stats__box--number"]} ${styles["stats__box--number--blue"]}`}>
+						130
+					</p>
+				</Card>
+				<Card className={styles["stats__box"]}>
+					<div className={styles["stats__box--text"]}>
+						<FontAwesomeIcon
+							icon={solid("check")}
+							className={styles["stats__icon"]}
+						/>
+						<p>Completed Tasks</p>
+					</div>
+					<p
+						className={`${styles["stats__box--number"]} ${styles["stats__box--number--purple"]}`}>
+						20
+					</p>
+				</Card>
+				<Card className={styles["stats__box"]}>
+					<div className={styles["stats__box--text"]}>
+						<FontAwesomeIcon
+							icon={solid("square-plus")}
+							className={styles["stats__icon"]}
+						/>
+						<p>New Tasks</p>
+					</div>
+					<p
+						className={`${styles["stats__box--number"]} ${styles["stats__box--number--green"]}`}>
+						7
+					</p>
+				</Card>
+				<Card className={styles["stats__box"]}>
+					<div className={styles["stats__box--text"]}>
+						<FontAwesomeIcon
+							icon={solid("fire-flame-curved")}
+							className={styles["stats__icon"]}
+						/>
+						<p>Habbits Streak</p>
+					</div>
+					<p
+						className={`${styles["stats__box--number"]} ${styles["stats__box--number--blue"]}`}>
+						4
 					</p>
 				</Card>
 			</section>
