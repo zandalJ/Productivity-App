e30fb9552b4708d9157a6fc6f0399745ec03b8fa
Fabian <zandal.zdl@gmail.com>
2023-02-20 23:29:43 +0100

    Added team mebers to dashboard page

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -2,10 +2,27 @@ import styles from "./Dashboard.module.scss";
 import Wrapper from "../Components/ui/Wrapper";
 import HeadingText from "../Components/ui/HeadingText";
 import Card from "../Components/ui/Card";
+import UserPhoto from "../Components/ui/UserPhoto";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import useWidth from "../hooks/useWidth";
 
 const DashboardPage = () => {
+	const width = useWidth();
+	let membersPhotoSize;
+	if (width < 576) {
+		membersPhotoSize = 35;
+	}
+	if (width >= 576) {
+		membersPhotoSize = 45;
+	}
+	if (width >= 992) {
+		membersPhotoSize = 35;
+	}
+	if (width > 1050) {
+		membersPhotoSize = 55;
+	}
+
 	return (
 		<Wrapper>
 			<HeadingText />
@@ -41,61 +58,166 @@ const DashboardPage = () => {
 					</p>
 				</Card>
 			</section>
-			<section className={`${styles.stats} ${styles.section}`}>
-				<h2>Tasks & Habits</h2>
-				<Card className={styles["stats__box"]}>
-					<div className={styles["stats__box--text"]}>
-						<FontAwesomeIcon
-							icon={solid("infinity")}
-							className={styles["stats__icon"]}
-						/>
-						<p>All Tasks</p>
-					</div>
-					<p
-						className={`${styles["stats__box--number"]} ${styles["stats__box--number--blue"]}`}>
-						130
-					</p>
-				</Card>
-				<Card className={styles["stats__box"]}>
-					<div className={styles["stats__box--text"]}>
-						<FontAwesomeIcon
-							icon={solid("check")}
-							className={styles["stats__icon"]}
-						/>
-						<p>Completed Tasks</p>
-					</div>
-					<p
-						className={`${styles["stats__box--number"]} ${styles["stats__box--number--purple"]}`}>
-						20
-					</p>
-				</Card>
-				<Card className={styles["stats__box"]}>
-					<div className={styles["stats__box--text"]}>
-						<FontAwesomeIcon
-							icon={solid("square-plus")}
-							className={styles["stats__icon"]}
-						/>
-						<p>New Tasks</p>
-					</div>
-					<p
-						className={`${styles["stats__box--number"]} ${styles["stats__box--number--green"]}`}>
-						7
-					</p>
-				</Card>
-				<Card className={styles["stats__box"]}>
-					<div className={styles["stats__box--text"]}>
-						<FontAwesomeIcon
-							icon={solid("fire-flame-curved")}
-							className={styles["stats__icon"]}
-						/>
-						<p>Habbits Streak</p>
-					</div>
-					<p
-						className={`${styles["stats__box--number"]} ${styles["stats__box--number--blue"]}`}>
-						4
-					</p>
-				</Card>
-			</section>
+			<div className={styles["section-bottom-wrapper"]}>
+				<section className={`${styles.stats} ${styles.section}`}>
+					<h2 className={styles.subheading}>Tasks & Habits</h2>
+					<Card className={`${styles["stats__box"]} ${styles["card-box"]}`}>
+						<div className={styles["stats__box--text"]}>
+							<FontAwesomeIcon
+								icon={solid("infinity")}
+								className={styles["stats__icon"]}
+							/>
+							<p>All Tasks</p>
+						</div>
+						<p
+							className={`${styles["stats__box--number"]} ${styles["stats__box--number--blue"]}`}>
+							130
+						</p>
+					</Card>
+					<Card className={`${styles["stats__box"]} ${styles["card-box"]}`}>
+						<div className={styles["stats__box--text"]}>
+							<FontAwesomeIcon
+								icon={solid("check")}
+								className={styles["stats__icon"]}
+							/>
+							<p>Completed Tasks</p>
+						</div>
+						<p
+							className={`${styles["stats__box--number"]} ${styles["stats__box--number--purple"]}`}>
+							20
+						</p>
+					</Card>
+					<Card className={`${styles["stats__box"]} ${styles["card-box"]}`}>
+						<div className={styles["stats__box--text"]}>
+							<FontAwesomeIcon
+								icon={solid("square-plus")}
+								className={styles["stats__icon"]}
+							/>
+							<p>New Tasks</p>
+						</div>
+						<p
+							className={`${styles["stats__box--number"]} ${styles["stats__box--number--green"]}`}>
+							7
+						</p>
+					</Card>
+					<Card className={`${styles["stats__box"]} ${styles["card-box"]}`}>
+						<div className={styles["stats__box--text"]}>
+							<FontAwesomeIcon
+								icon={solid("fire-flame-curved")}
+								className={styles["stats__icon"]}
+							/>
+							<p>Habbits Streak</p>
+						</div>
+						<p
+							className={`${styles["stats__box--number"]} ${styles["stats__box--number--blue"]}`}>
+							4
+						</p>
+					</Card>
+				</section>
+				<section className={`${styles.members} ${styles.section}`}>
+					<h2 className={styles.subheading}>Team Members</h2>
+					<Card className={`${styles["members__box"]} ${styles["card-box"]}`}>
+						<div className={styles["members__member"]}>
+							<UserPhoto
+								href='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+								size={membersPhotoSize}
+							/>
+							<div className={styles["members__member__data"]}>
+								<p className={styles["members__member__names"]}>
+									Leila Coleman
+								</p>
+								<p className={styles["members__member__email"]}>
+									leila.coleman@gmail.com
+								</p>
+							</div>
+						</div>
+						<div className={styles["members__member"]}>
+							<UserPhoto
+								href='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+								size={membersPhotoSize}
+							/>
+							<div className={styles["members__member__data"]}>
+								<p className={styles["members__member__names"]}>
+									Leila Coleman
+								</p>
+								<p className={styles["members__member__email"]}>
+									leila.coleman@gmail.com
+								</p>
+							</div>
+						</div>
+						<div className={styles["members__member"]}>
+							<UserPhoto
+								href='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+								size={membersPhotoSize}
+							/>
+							<div className={styles["members__member__data"]}>
+								<p className={styles["members__member__names"]}>
+									Leila Coleman
+								</p>
+								<p className={styles["members__member__email"]}>
+									leila.coleman@gmail.com
+								</p>
+							</div>
+						</div>
+						<div className={styles["members__member"]}>
+							<UserPhoto
+								href='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+								size={membersPhotoSize}
+							/>
+							<div className={styles["members__member__data"]}>
+								<p className={styles["members__member__names"]}>
+									Leila Coleman
+								</p>
+								<p className={styles["members__member__email"]}>
+									leila.coleman@gmail.com
+								</p>
+							</div>
+						</div>
+						<div className={styles["members__member"]}>
+							<UserPhoto
+								href='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+								size={membersPhotoSize}
+							/>
+							<div className={styles["members__member__data"]}>
+								<p className={styles["members__member__names"]}>
+									Leila Coleman
+								</p>
+								<p className={styles["members__member__email"]}>
+									leila.coleman@gmail.com
+								</p>
+							</div>
+						</div>
+						<div className={styles["members__member"]}>
+							<UserPhoto
+								href='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+								size={membersPhotoSize}
+							/>
+							<div className={styles["members__member__data"]}>
+								<p className={styles["members__member__names"]}>
+									Leila Coleman
+								</p>
+								<p className={styles["members__member__email"]}>
+									leila.coleman@gmail.com
+								</p>
+							</div>
+						</div>
+						<div className={styles["members__member"]}>
+							<UserPhoto
+								href='https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80'
+								size={membersPhotoSize}
+							/>
+							<div className={styles["members__member__data"]}>
+								<p className={styles["members__member__names"]}>
+									Leila Coleman
+								</p>
+								<p className={styles["members__member__email"]}>
+									leila.coleman@gmail.com
+								</p>
+							</div>
+						</div>
+					</Card>
+				</section>
+			</div>
 		</Wrapper>
 	);
 };
