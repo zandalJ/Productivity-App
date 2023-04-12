41b2e6dc7d1e91181af79f8396c32ae9fbff7398
Fabian <zandal.zdl@gmail.com>
2023-04-12 20:28:02 +0200

    Fix

--- src/Components/habits/DaySlider.jsx
+++ src/Components/habits/DaySlider.jsx
@@ -1,44 +1,72 @@
-import { useRef } from "react";
+import { useState, useEffect } from "react";
 import DayCircle from "./DayCircle";
 import moment from "moment";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 
-const DaySlider = () => {
-	const swiperRef = useRef(null);
-	const currentDay = parseInt(moment().format("D"));
-	const sliderElements = [];
 
-	const activeSlideHandler = index => {
-		console.log(index);
-		swiperRef.current?.slider.slideTo(index);
+const DaySlider = () => {
+	const [slides, setSlides] = useState([]);
+	const [swiper, setSwiper] = useState(null);
+	const currentDay = parseInt(moment().format("D"));
+
+	const activeSlideHandler = activeIndex => {
+		console.log(swiper);
+		swiper.slideTo(activeIndex-1)
+		const updatedSlides = slides.map((slide, index) => {
+			let key = index+1
+			if (key !== activeIndex) {
+				return (
+					<SwiperSlide key={key} onClick={() => activeSlideHandler(key)}>
+						<DayCircle number={key} />
+					</SwiperSlide>
+				);
+			}else{
+				return (
+					<SwiperSlide key={key} onClick={() => activeSlideHandler(key)}>
+						<DayCircle number={key} active/>
+					</SwiperSlide>
+				);
+			}
+		})
+		setSlides(updatedSlides)
 	};
 
-	for (let i = 1; i < 32; i++) {
-		if (i !== currentDay) {
-			sliderElements.push(
-				<SwiperSlide key={i} onClick={() => activeSlideHandler(i)}>
-					<DayCircle number={i} />
-				</SwiperSlide>
-			);
-		} else {
-			sliderElements.push(
-				<SwiperSlide key={i} onClick={() => activeSlideHandler(i)}>
-					<DayCircle number={i} active />
-				</SwiperSlide>
-			);
+	useEffect(() => {
+		if(swiper){
+		const sliderElements = [];
+		for (let i = 1; i < 32; i++) {
+			if (i !== currentDay) {
+				const el = (
+					<SwiperSlide key={i} onClick={() => activeSlideHandler(i)}>
+						<DayCircle number={i} />
+					</SwiperSlide>
+				);
+				sliderElements.push(el);
+			} else {
+				const el = (
+					<SwiperSlide key={i} onClick={() => activeSlideHandler(i)}>
+						<DayCircle number={i} active />
+					</SwiperSlide>
+				);
+				sliderElements.push(el)
+			}
 		}
+		setSlides([...sliderElements]);
 	}
+	}, [swiper]);
+
+
 
 	const sliderParams = {
 		initialSlide: currentDay - 1,
 		slidesPerView: 7,
 		centeredSlides: true,
 		centeredSlidesBounds: true,
-		onClick: e => console.log(e.target),
+		onSwiper: swiper => setSwiper(swiper),
 	};
 
-	return <Swiper ref={swiperRef} {...sliderParams}>{sliderElements}</Swiper>;
+	return <Swiper {...sliderParams}>{slides}</Swiper>;
 };
 
 export default DaySlider;
