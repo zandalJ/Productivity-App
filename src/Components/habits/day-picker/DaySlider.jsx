41f9f490c04bae709194e9a9b600de2723d339ce
Fabian <zandal.zdl@gmail.com>
2023-04-17 23:19:29 +0200

    Half-fixed swiper date picker

--- src/Components/habits/day-picker/DaySlider.jsx
+++ src/Components/habits/day-picker/DaySlider.jsx
@@ -6,109 +6,164 @@ import "swiper/css";
 
 const DaySlider = () => {
 	const [slides, setSlides] = useState([]);
-	const [swiper, setSwiper] = useState(null);
+	const [swiper, setSwiper] = useState({});
 	const currentDay = parseInt(moment().format("D"));
 
-	const getSlidesHandler = useCallback((manualActiveIndex = null) => {
-		const activeIndex = manualActiveIndex || currentDay;
-		const sliderElements = [];
-		let subtractionNum = activeIndex - 30;
-
-		const prevMonth = moment().subtract(1, "month").month();
-		const year = moment().year();
-		const date = moment({ year, month: prevMonth });
-		const monthDays = date.daysInMonth();
-
-		const getStartIndex = () => {
-			if (subtractionNum < 0) {
-				return { index: monthDays + subtractionNum, currentMonth: false };
-			} else if (subtractionNum === 0) {
-				return { index: monthDays, currentMonth: false };
-			} else {
-				return { index: subtractionNum, currentMonth: true };
+	const activeSlideHandler = useCallback(
+		activeIndex => {
+			if (swiper.initialized && slides.length>0) {
+				console.log(activeIndex);
+				console.log('ok');
+				swiper.slideTo(activeIndex);
 			}
-		};
+		},
+		[swiper, slides]
+	);
 
-		const startIndex = getStartIndex();
+	const getSlidesHandler = useCallback(
+		(manualActiveIndex = null) => {
+			const activeIndex = manualActiveIndex || currentDay;
+			const sliderElements = [];
+			let subtractionNum = activeIndex - 14;
 
-		for (let i = startIndex.index; i < startIndex.index + 30; i++) {
-			if (!startIndex.currentMonth) {
-				if (i <= monthDays) {
-					console.log('ok');
-					const el = (
-						<SwiperSlide key={i} onClick={() => activeSlideHandler(i)}>
-							<DayCircle number={i} />
-						</SwiperSlide>
-					);
-					sliderElements.push(el);
+			const prevMonth = moment().subtract(1, "month").month();
+			const year = moment().year();
+			const date = moment({ year, month: prevMonth });
+			const monthDays = date.daysInMonth();
+
+			const getStartIndex = () => {
+				if (subtractionNum < 0) {
+					return { index: monthDays + subtractionNum, currentMonth: false };
+				} else if (subtractionNum === 0) {
+					return { index: monthDays, currentMonth: false };
 				} else {
-					let newMonthIndex = i - monthDays;
-					const el = (
-						<SwiperSlide
-							key={newMonthIndex}
-							onClick={() => activeSlideHandler(newMonthIndex)}>
-							<DayCircle number={newMonthIndex} />
-						</SwiperSlide>
-					);
-					sliderElements.push(el);
+					return { index: subtractionNum, currentMonth: true };
 				}
-			} else {
-				const el = (
-					<SwiperSlide key={i} onClick={() => activeSlideHandler(i)}>
-						<DayCircle number={i} />
-					</SwiperSlide>
-				);
-				sliderElements.push(el);
+			};
+
+			const startIndex = getStartIndex();
+			for (
+				let i = startIndex.index;
+				i < startIndex.index + 29 + startIndex.index;
+				i++
+			) {
+				if (i < startIndex.index + startIndex.index) {
+					sliderElements.push(null);
+				} else {
+					let dayNumber = i - startIndex.index;
+					if (!startIndex.currentMonth) {
+						if (dayNumber < monthDays) {
+							const el = (
+								<SwiperSlide
+									key={dayNumber}
+									onClick={() => activeSlideHandler(dayNumber)}>
+									<DayCircle number={dayNumber} />
+								</SwiperSlide>
+							);
+							sliderElements.push(el);
+						} else {
+							let newMonthIndex = dayNumber - monthDays;
+							const el = (
+								<SwiperSlide
+									key={newMonthIndex}
+									onClick={() => activeSlideHandler(newMonthIndex)}>
+									<DayCircle number={newMonthIndex} />
+								</SwiperSlide>
+							);
+							sliderElements.push(el);
+						}
+					} else {
+						const el = (
+							<SwiperSlide
+								key={dayNumber}
+								onClick={() => activeSlideHandler(dayNumber)}>
+								<DayCircle number={dayNumber} />
+							</SwiperSlide>
+						);
+						sliderElements.push(el);
+					}
+				}
 			}
-		}
 
-		const outputSlider = sliderElements.map((el, index) => {
-			let key = parseInt(el.key) + 1;
-			if (key !== activeIndex) {
-				return (
-					<SwiperSlide key={key} onClick={() => activeSlideHandler(key)}>
-						<DayCircle number={key} />
-					</SwiperSlide>
-				);
-			} else {
-				return (
-					<SwiperSlide key={key} onClick={() => activeSlideHandler(key)}>
-						<DayCircle number={key} active />
-					</SwiperSlide>
-				);
+			const outputSlider = sliderElements.map((el, index) => {
+				if (el !== null) {
+					let key = parseInt(el.key) + 1;
+					if (key !== activeIndex) {
+						return (
+							<SwiperSlide key={key} onClick={() => activeSlideHandler(index)}>
+								<DayCircle number={key} />
+							</SwiperSlide>
+						);
+					} else {
+						return (
+							<SwiperSlide key={key} onClick={() => activeSlideHandler(index)}>
+								<DayCircle number={key} active />
+							</SwiperSlide>
+						);
+					}
+				} else {
+					return null;
+				}
+			});
+
+			if (swiper.initialized) {
+				setSlides(outputSlider);
 			}
-		});
-
-		console.log(outputSlider);
-		setSlides(outputSlider);
-	}, []);
-
-	const activeSlideHandler = activeIndex => {
-		swiper.slideTo(activeIndex - 1);
-		getSlidesHandler(activeIndex);
-	};
+		},
+		[currentDay, activeSlideHandler, swiper.initialized]
+	);
 
 	useEffect(() => {
-		if (swiper) {
-			getSlidesHandler();
+		if (swiper && !swiper.destroyed) {
+			if (slides.length === 0) {
+				getSlidesHandler();
+			}
 		}
-	}, [swiper]);
+	}, [swiper, getSlidesHandler, slides.length]);
+
+	const initSwiperHandler = useCallback(swiper => {
+		setSwiper(swiper);
+	}, []);
+
+	// const slidesHandler = () => {
+	// 	if (slides.length > 0) {
+	// 		console.log(swiper.activeIndex);
+	// 		console.log(parseInt(slides[swiper.activeIndex].key));
+	// 		getSlidesHandler(parseInt(slides[swiper.activeIndex].key));
+	// 		console.log(swiper);
+	// 		console.log(slides);
+	// 	}
+	// };
+
+	useEffect(() => {
+		console.log(swiper);
+	}, [swiper])
 
 	useEffect(() => {
 		console.log(slides);
-	}, [slides]);
+	}, [slides])
 
 	const sliderParams = {
-		initialSlide: currentDay - 1,
+		initialSlide: 13,
 		slidesPerView: 7,
 		centeredSlides: true,
 		centeredSlidesBounds: true,
-		onSwiper: swiper => setSwiper(swiper),
+		slideToClickedSlide: true,
+		onSwiper: swiper => initSwiperHandler(swiper),
+		// onSlideChange: () => slidesHandler(),
 	};
 
-	return <Swiper {...sliderParams}>{slides}</Swiper>;
+	return (
+		<Swiper {...sliderParams}>
+			{swiper.initialized &&
+				slides.map(item => {
+					if (item === null) {
+						return null;
+					}
+					return item;
+				})}
+		</Swiper>
+	);
 };
 
 export default DaySlider;
-
-// ugulem dziala tylko z liczbami cos sie jebie tzn jest dzien nr2 a pozniej nagle 32 (bez 1) i na slidera wypierdala za kazdym kliknieciu elementa
