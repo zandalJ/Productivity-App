48116e2beb21ca9c0c430613652fc96bc9e9df00
Fabian <zandal.zdl@gmail.com>
2023-05-23 23:36:48 +0200

    Fix

--- src/Components/user/UserNavInfo.jsx
+++ src/Components/user/UserNavInfo.jsx
@@ -6,9 +6,10 @@ import anonymousAvatar from "../../img/anonymous-avatar.png";
 const UserNavInfo = ({ open, animationVariants, photoSize }) => {
 	const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
 	const userData = useSelector(state => state.auth.userData);
-	const name = isLoggedIn
-		? `${userData.name} ${userData.surname}`
-		: "Anonymous";
+	const name =
+		isLoggedIn && userData
+			? `${userData.name} ${userData.surname}`
+			: "Anonymous";
 	const nick = isLoggedIn ? `@${userData.nickname}` : "@anonym";
 	const photoUrl = isLoggedIn
 		? "https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
